a46547f5e0cdf667e50d9a441914d1446e58ba78
Larissa <lariellen67@gmail.com>
2019-10-29 17:01:10 -0300

    up

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,6 +3,10 @@ import React, {Component} from 'react';
 import{ View, Text } from 'react-native';
 
 export default class Main extends Component{
+    static navigationOptions = { // adiciona título ao app
+        title: "JSHunt"
+    };
+    
     render(){
         return(
             <View>
